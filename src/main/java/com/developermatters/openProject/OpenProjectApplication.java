312db01003549965127a92fdf825980dc624596b
vignesh-ragavan <vigneshragavan1@gmail.com>
2023-05-28 04:53:24 +0530

    initial setup@

---
+++ src/main/java/com/developermatters/openProject/OpenProjectApplication.java
@@ -0,0 +1,13 @@
+package com.developermatters.openProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OpenProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OpenProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/developermatters/openProject/OpenProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.developermatters.openProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OpenProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
